d47bb0ba18face2c703d15aa96a24b342ed68cd7
nicksan4ez <san4ez.nick@gmail.com>
2026-01-23 00:54:53 +0300

    Почистил от лишних папок и файлов

--- tests/test_main_helpers.py
+++
@@ -1,7 +0,0 @@
-from src.main import build_switch_pm_text
-
-
-def test_build_switch_pm_text_length() -> None:
-    text = build_switch_pm_text()
-    assert text == "Найти и подготовить 🎬 ≈ 10 сек"
-
